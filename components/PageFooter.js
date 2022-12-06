233d2eb989f6154336a45348f3da667f4b1ea571
Andrew D <56039221+andent2@users.noreply.github.com>
2022-12-05 21:11:38 -0500

    Add page footer component

---
+++ components/PageFooter.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  background-color: var(--page-footer-background-color);
+  color: var(--text-color);
+
+  height: 4rem;
+  width: 100%;
+  max-width: 1200px;
+
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const PageFooter = () => {
+  return (
+    <Footer>
+      <p>footer link</p>
+      <p>footer link</p>
+      <p>footer link</p>
+    </Footer>
+  );
+};
+
+export default PageFooter;
