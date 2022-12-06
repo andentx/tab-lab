1d0a6f60708ae6ff35f9a4c5d0b37e0cd55d1db3
Andrew D <56039221+andent2@users.noreply.github.com>
2022-12-05 21:32:01 -0500

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,20 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
-import styled from 'styled-components';
-
-export const PreviewContainer = styled.div`
-  background-color: hotpink;
-  height: 200px;
-  width: 200px;
-`;
+import Layout from '../components/layout';
+import Title from '../components/Title';
+import PagePreview from '../components/PagePreview';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>tab lab - development</title>
         <meta name='description' content='tab lab - development' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>tab lab</h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-        <PreviewContainer>preview container</PreviewContainer>
-      </main>
-
-      <footer className={styles.footer}></footer>
-    </div>
+      <Title />
+      <PagePreview />
+    </Layout>
   );
 }
