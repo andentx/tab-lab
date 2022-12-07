ef78444d79e18b12b5336ebfe18cc32a4973f9a6
Andrew D <56039221+andent2@users.noreply.github.com>
2022-12-07 16:50:16 -0500

    Update forms

--- components/SettingsSection.js
+++ components/SettingsSection.js
@@ -2,6 +2,21 @@ import styled from 'styled-components';
 
 import TextField from '@mui/material/TextField';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#ff0000', // color of outline and label when focused
+    },
+    text: {
+      primary: '#ffa500', // color of text inside of form when not focused
+      secondary: '#ffff00', // color of label text when not focused
+    },
+  },
+});
+
 const SettingsSectionContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -11,51 +26,89 @@ const SettingsSectionContainer = styled.div`
   min-height: 5rem;
 
   margin-bottom: 15rem;
+
+  .MuiOutlinedInput-notchedOutline {
+    border: 2px solid red; // color of notched outline when not focused
+    border-radius: 0;
+  }
+
+  .MuiNativeSelect-icon,
+  .MuiSelect-icon {
+    fill: red;
+  }
 `;
 
 const SettingsSection = () => {
   return (
-    <SettingsSectionContainer>
-      <TextField
-        id='example1'
-        select
-        fullWidth
-        margin='normal'
-        label='Example 1'
-        value='Example 1'
-        SelectProps={{
-          native: true,
-        }}
-      >
-        ` <option>Example 1 - MUI - TextField - Select - Native</option>
-      </TextField>
-      <TextField
-        id='example2'
-        select
-        fullWidth
-        margin='normal'
-        label='Example 2'
-        value='Example 2'
-        SelectProps={{
-          native: true,
-        }}
-      >
-        ` <option>Example 2 - MUI - TextField - Select - Native</option>
-      </TextField>
-      <TextField
-        id='example3'
-        select
-        fullWidth
-        margin='normal'
-        label='Example 3'
-        value='Example 3'
-        SelectProps={{
-          native: true,
-        }}
-      >
-        ` <option>Example 3 - MUI - TextField - Select - Native</option>
-      </TextField>
-    </SettingsSectionContainer>
+    <ThemeProvider theme={theme}>
+      <SettingsSectionContainer>
+        <TextField
+          id='example1'
+          select
+          fullWidth
+          margin='normal'
+          label='Example 1'
+          value='Example 1'
+          SelectProps={{
+            native: true,
+          }}
+        >
+          <option>Example 1 - MUI - TextField - Select - Native</option>
+        </TextField>
+        <TextField
+          id='example1'
+          select
+          fullWidth
+          margin='normal'
+          label='Example 1'
+          value='Example 1'
+          SelectProps={{
+            native: true,
+          }}
+        >
+          <option>Example 1 - MUI - TextField - Select - Native</option>
+        </TextField>
+        <TextField
+          id='example1'
+          select
+          fullWidth
+          margin='normal'
+          label='Example 1'
+          value='Example 1'
+          SelectProps={{
+            native: true,
+          }}
+        >
+          <option>Example 1 - MUI - TextField - Select - Native</option>
+        </TextField>
+        <TextField
+          id='example1'
+          select
+          fullWidth
+          margin='normal'
+          label='Example 1'
+          value='Example 1'
+          SelectProps={{
+            native: false,
+          }}
+        >
+          <option>Example 1 - MUI - TextField - Select - Native</option>
+        </TextField>
+        <TextField
+          id='example1'
+          select
+          fullWidth
+          margin='normal'
+          label='Example 1'
+          value='Example 1'
+          SelectProps={{
+            native: true,
+          }}
+        >
+          <option>Example 1 - MUI - TextField - Select - Native</option>
+        </TextField>
+      </SettingsSectionContainer>
+    </ThemeProvider>
   );
 };
 
