32afd4375fdcdfbf092d4a8e2b111511a9e19eae
Andrew D <56039221+andent2@users.noreply.github.com>
2022-12-08 14:00:30 -0500

    Update forms

--- components/SettingsSection.js
+++ components/SettingsSection.js
@@ -1,21 +1,6 @@
 import styled from 'styled-components';
 
-import TextField from '@mui/material/TextField';
-
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: 'hsl(34, 78%, 91%)', // color of outline and label when focused
-    },
-    text: {
-      primary: 'hsl(34, 78%, 91%)', // color of text inside of form when not focused
-      secondary: 'hsl(34, 78%, 91%)', // color of label text when not focused
-    },
-  },
-});
+import { useState } from 'react';
 
 const SettingsSectionContainer = styled.div`
   display: flex;
@@ -26,19 +11,9 @@ const SettingsSectionContainer = styled.div`
   min-height: 5rem;
 
   margin-bottom: 15rem;
-
-  .MuiOutlinedInput-notchedOutline {
-    border: 2px solid hsl(34, 78%, 91%); // color of notched outline when not focused
-    border-radius: 0;
-  }
-
-  .MuiNativeSelect-icon,
-  .MuiSelect-icon {
-    fill: hsl(34, 78%, 91%);
-  }
 `;
 
-const MyCustomForm = styled.div`
+const CustomSelect = styled.div`
   /* background-color: darkred; */
 
   display: flex;
@@ -91,45 +66,118 @@ const MyCustomForm = styled.div`
 `;
 
 const SettingsSection = () => {
+  const [settings, setSettings] = useState({});
+
+  const handleChange = (event) => {
+    setSettings({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <SettingsSectionContainer>
-        <TextField
-          id='example1'
-          select
-          fullWidth
-          margin='normal'
-          label='String Number'
-          value='Example 1'
-          SelectProps={{
-            native: true,
-          }}
-        >
-          <option>Example 1 - MUI - TextField - Select - Native</option>
-          <option>Example 1 - MUI - TextField - Select - Native</option>
-          <option>Example 1 - MUI - TextField - Select - Native</option>
-        </TextField>
-        <MyCustomForm>
-          <label htmlFor='selectElement'>String Number</label>
-          <select id='selectElement'>
-            <option>Example 2 - Custom</option>
-            <option>Example 2 - Custom</option>
-            <option>Example 2 - Custom</option>
+    <SettingsSectionContainer>
+      <form>
+        <input type='text' name='name' onChange={handleChange} />
+
+        <CustomSelect>
+          <label htmlFor='numberOfStrings'>Number of Strings</label>
+          <select id='numberOfStrings'>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
           </select>
           <svg width='24' height='24' viewBox='0 0 24 24'>
             <path d='M12 21l-12-18h24z' />
           </svg>
-        </MyCustomForm>
-        <div>
-          <label htmlFor='selectElement'>String Number</label>
-          <select id='selectElement'>
-            <option>Example 2 - Custom</option>
-            <option>Example 2 - Custom</option>
-            <option>Example 2 - Custom</option>
+        </CustomSelect>
+        <CustomSelect>
+          <label htmlFor='numberOfRows'>Number of Rows</label>
+          <select id='numberOfRows'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
           </select>
-        </div>
-      </SettingsSectionContainer>
-    </ThemeProvider>
+          <svg width='24' height='24' viewBox='0 0 24 24'>
+            <path d='M12 21l-12-18h24z' />
+          </svg>
+        </CustomSelect>
+        <CustomSelect>
+          <label htmlFor='numberOfMeasureLines'>Number of Measure Lines</label>
+          <select id='numberOfMeasureLines'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
+          </select>
+          <svg width='24' height='24' viewBox='0 0 24 24'>
+            <path d='M12 21l-12-18h24z' />
+          </svg>
+        </CustomSelect>
+        <CustomSelect>
+          <label htmlFor='offset'>Offset for 3-Ring Binder</label>
+          <select id='offset'>
+            <option>Offset for 3-Ring Binder</option>
+            <option>Centered</option>
+          </select>
+          <svg width='24' height='24' viewBox='0 0 24 24'>
+            <path d='M12 21l-12-18h24z' />
+          </svg>
+        </CustomSelect>
+        <CustomSelect>
+          <label htmlFor='pageHeading'>Heading</label>
+          <select id='pageHeading'>
+            <option>None</option>
+            <option>Title</option>
+            <option>Title + BPM + Key</option>
+          </select>
+          <svg width='24' height='24' viewBox='0 0 24 24'>
+            <path d='M12 21l-12-18h24z' />
+          </svg>
+        </CustomSelect>
+        <CustomSelect>
+          <label htmlFor='horizontalAdjust'>Horizontal Adjust</label>
+          <select id='horizontalAdjust'>
+            <option>100%</option>
+          </select>
+          <svg width='24' height='24' viewBox='0 0 24 24'>
+            <path d='M12 21l-12-18h24z' />
+          </svg>
+        </CustomSelect>
+        <CustomSelect>
+          <label htmlFor='verticalAdjust'>Vertical Adjust</label>
+          <select id='verticalAdjust'>
+            <option>100%</option>
+          </select>
+          <svg width='24' height='24' viewBox='0 0 24 24'>
+            <path d='M12 21l-12-18h24z' />
+          </svg>
+        </CustomSelect>
+      </form>
+    </SettingsSectionContainer>
   );
 };
 
