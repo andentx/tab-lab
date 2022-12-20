2f6fb0cc6f58d9dcd9da5ff1701bc3fd55cc6291
Andrew D <56039221+andent2@users.noreply.github.com>
2022-12-20 13:12:42 -0500

    Doesn't work

--- components/PageGenerator.js
+++ components/PageGenerator.js
@@ -29,7 +29,6 @@ const CellsContainer = styled.div`
 
   width: 80%;
   height: 10%;
-  /* aspect-ratio: 8/1; */
 `;
 
 const Cell = styled.div`
@@ -38,9 +37,11 @@ const Cell = styled.div`
 
 const PageGenerator = () => {
   const [pageSettings, setPageSettings] = useState({
-    numberOfStrings: 6,
-    numberOfVerticalLines: 8,
-    numberOfRows: 6,
+    numberOfStrings: 6, // duplicate?
+    numberOfVerticalLines: 8, // duplicate?
+    numberOfColumns: 8, // duplicate?
+    numberOfRows: 6, // duplicate?
+    numberOfCellContainers: 5,
     horizontalAdjust: 100,
     verticalAdjust: 100,
     horizontalOffset: 0,
@@ -53,157 +54,74 @@ const PageGenerator = () => {
 
   const [cellHeight, setCellHeight] = useState();
   const [cellWidth, setCellWidth] = useState();
-  const [numberOfRows, setNumberOfRows] = useState(5);
-  // const [numberOfStrings, setNumberOfStrings] = useState(6);
-  const [numberOfColumns, setNumberOfColumns] = useState(8);
-  const [numberOfCellContainers, setNumberOfCellContainers] = useState(7);
-  const [arrayOfCellsInState, setArrayOfCellsInState] = useState([]);
-  const [arrayOfCellContainersInState, setArrayOfCellContainersInState] = useState([]);
-  const [horizontalAdjust, setHorizontalAdjust] = useState(100);
-  const [verticalAdjust, setVerticalAdjust] = useState(100);
-  const [gapBetweenTabRows, setGapBetweenTabRows] = useState(5);
-  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
-  const [headerDisplayProperty, setHeaderDisplayProperty] = useState('flex');
-  const [headerHeight, setHeaderHeight] = useState(10);
-  const [borderSize, setBorderSize] = useState(2);
-  const [borderSizeValue, setBorderSizeValue] = useState(1);
-  const [steplistValue, setSteplistValue] = useState(6);
-
-  const [tabRowWidth, setTabRowWidth] = useState(80);
-  const [tabRowHeight, setTabRowHeight] = useState(10);
-
-  const [leftOffset, setLeftOffset] = useState(0);
-
-  function handleSteplistValueChange(event) {
-    let numberOfRowsShouldBe = event.target.value / 12.5 + 3;
-
-    setSteplistValue(numberOfRowsShouldBe + 1);
-    setNumberOfRows(numberOfRowsShouldBe);
-  }
-
-  function handleNumberOfRowsChange(event) {
-    setNumberOfRows(event.target.value);
-  }
-
-  function handleNumberOfStringsChange(event) {
-    setNumberOfStrings(event.target.value);
-
-    setNumberOfRows(event.target.value - 1);
-  }
-
-  function handleNumberOfColumnsChange(event) {
-    setNumberOfColumns(event.target.value);
-  }
-  function handleNumberOfCellContainersChange(event) {
-    setNumberOfCellContainers(event.target.value);
-  }
-  function handleHorizontalAdjustChange(event) {
-    setHorizontalAdjust(event.target.value);
-    setTabRowWidth(event.target.value - 20);
-  }
-
-  function handleLeftOffsetChange(event) {
-    setLeftOffset(event.target.value);
-  }
-
-  function handleVerticalAdjustChange(event) {
-    setVerticalAdjust(event.target.value);
-    setTabRowHeight(event.target.value / 10);
-  }
-
-  function handleGapBetweenTabRowsChange(event) {
-    setGapBetweenTabRows(event.target.value);
-  }
-
-  function handleBorderSizeChange(event) {
-    setBorderSize(event.target.value);
-    setBorderSizeValue(event.target.value / 2);
-  }
-
-  function handleIsHeaderVisibleChange(event) {
-    setIsHeaderVisible(event.target.value);
-
-    if (event.target.value == 'true') {
-      setHeaderDisplayProperty('flex');
-    }
-    if (event.target.value == 'false') {
-      setHeaderDisplayProperty('none');
-    }
-  }
-
-  function handleHeaderHeightChange(event) {
-    setHeaderHeight(event.target.value);
-  }
+  const [cells, setCells] = useState([]);
+  const [cellContainers, setCellContainers] = useState([]);
+  const [cellContainerHeight, setCellContainerWidth] = useState(80);
+  const [cellContainerWidth, setCellContainerHeight] = useState(10);
 
   function handleSettingsChange(event) {
     const { name, value } = event.target;
     setPageSettings((previousSettings) => ({ ...previousSettings, [name]: value }));
   }
 
-  useEffect(() => {
-    createArrayOfCells(numberOfRows, numberOfColumns);
-
-    function createArrayOfCells(rows, cols) {
-      let calculatedCellHeight = 100 / rows;
-      let calculatedCellWidth = 100 / cols;
-
-      setCellHeight(calculatedCellHeight);
-      setCellWidth(calculatedCellWidth);
-
-      let numberOfCellsThatShouldBeCreated = rows * cols;
-
-      let newArrayOfCells = [];
-
-      for (var i = 0; i < numberOfCellsThatShouldBeCreated; i++) {
-        newArrayOfCells.push({ label: i, value: i, key: i });
-      }
-
-      setArrayOfCellsInState(newArrayOfCells);
+  function createCells(rows, cols) {
+    setCellHeight(100 / rows);
+    setCellWidth(100 / cols);
+    let newCells = [];
+    for (var i = 0; i < rows * cols; i++) {
+      console.log(`hi from 72`);
+      newCells.push({ label: i, value: i, key: i });
     }
-  }, [numberOfRows, numberOfColumns]);
+    setCells(newCells);
+  }
+
+  function createCellContainers(number) {
+    setPageSettings((previousSettings) => ({
+      ...previousSettings,
+      numberOfCellContainers: number,
+    }));
+    let newCellContainers = [];
+    for (var i = 0; i < number; i++) {
+      console.log(`hi from 85`);
+
+      newCellContainers.push({ label: i, value: i, key: i });
+    }
+    setCellContainers(newCellContainers);
+  }
 
   useEffect(() => {
-    createArrayOfCellContainers(pageSettings.numberOfRows);
+    createCellContainers(pageSettings.numberOfCellContainers);
+  }, [pageSettings.numberOfCellContainers]);
 
-    function createArrayOfCellContainers(number) {
-      setNumberOfCellContainers(number);
-      let numberOfCellContainersThatShouldBeCreated = number;
-
-      let newArrayOfCellContainers = [];
-
-      for (var i = 0; i < numberOfCellContainersThatShouldBeCreated; i++) {
-        newArrayOfCellContainers.push({ label: i, value: i, key: i });
-      }
-
-      setArrayOfCellContainersInState(newArrayOfCellContainers);
-    }
+  useEffect(() => {
+    createCells(pageSettings.numberOfRows, pageSettings.numberOfColumns);
   }, [pageSettings]);
 
-  const allCellsRendered = arrayOfCellsInState.map((cell) => (
+  const allCellsRendered = cells.map((cell) => (
     <Cell
-      className='classsss'
+      className='classsss' // fix this
       key={cell.key}
       id={cell['key']}
       style={{
         height: `${cellHeight}%`,
         width: `${cellWidth}%`,
-        border: `${borderSizeValue}px solid black`,
+        border: `${pageSettings.borderSize / 2}px solid black`,
       }}
     >
       {cell.name}
     </Cell>
   ));
 
-  const allCellContainersRendered = arrayOfCellContainersInState.map((cellContainer) => (
+  const allCellContainersRendered = cellContainers.map((cellContainer) => (
     <CellsContainer
       className='cellContainer'
       key={cellContainer.key}
       style={{
-        width: `${tabRowWidth}%`,
-        height: `${tabRowHeight}%`,
-        marginLeft: `${leftOffset}%`,
-        marginBottom: `${gapBetweenTabRows}%`,
-        border: `${borderSizeValue}px solid black`,
+        width: `${cellContainerWidth}%`,
+        height: `${cellContainerHeight}%`,
+        marginLeft: `${pageSettings.horizontalOffset}%`,
+        marginBottom: `${pageSettings.rowGap}%`,
+        border: `${pageSettings.borderSize / 2}px solid black`,
       }}
       id={cellContainer['key']}
     >
@@ -213,7 +131,7 @@ const PageGenerator = () => {
 
   return (
     <PageGeneratorContainer>
-      <PagePreview pageSettings={pageSettings} allCellContainersRendered={allCellContainersRendered}></PagePreview>
+      <PagePreview pageSettings={pageSettings} cellContainers={allCellContainersRendered}></PagePreview>
       <FormSection pageSettings={pageSettings} onInputChange={handleSettingsChange}></FormSection>
     </PageGeneratorContainer>
   );
