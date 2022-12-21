b053b18ef6ce2e9c1d25819771e5602ab7fe487c
Andrew D <56039221+andent2@users.noreply.github.com>
2022-12-21 10:59:49 -0500

    Add temp size

--- components/PagePreview.js
+++ components/PagePreview.js
@@ -7,11 +7,20 @@ const Page = styled.div`
 
   color: var(--text-color);
 
-  width: min(80%, 500px);
-  aspect-ratio: 8.5/11;
+  /* width: min(80%, 500px); */
+  /* aspect-ratio: 8.5/11; */
 
   /* width: 800px; */
   /* height: 1100px; */
+
+  /* width: 400px; */
+  /* height: 550px; */
+
+  width: 300px;
+  height: 385px;
+
+  /* width: 200px; */
+  /* height: 275px; */
 
   display: flex;
   flex-direction: column;
