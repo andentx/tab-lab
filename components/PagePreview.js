35553e0bf36cd822e13307d28ebefd0cc84e3cf2
Andrew D <56039221+andent2@users.noreply.github.com>
2022-12-21 21:26:34 -0500

    Update page preview

--- components/PagePreview.js
+++ components/PagePreview.js
@@ -7,27 +7,72 @@ const Page = styled.div`
 
   color: var(--text-color);
 
+  /* responsive version */
   /* width: min(80%, 500px); */
   /* aspect-ratio: 8.5/11; */
 
-  /* width: 800px; */
-  /* height: 1100px; */
+  @media (min-width: 200px) {
+    width: 170px;
+    height: 220px;
+  }
 
-  /* width: 400px; */
-  /* height: 550px; */
+  @media (min-width: 300px) {
+    width: 255px;
+    height: 330px;
+  }
 
-  width: 300px;
-  height: 385px;
+  @media (min-width: 400px) {
+    width: 340px;
+    height: 440px;
+  }
 
-  /* width: 200px; */
-  /* height: 275px; */
+  @media (min-width: 500px) {
+    width: 425px;
+    height: 550px;
+  }
+
+  @media (min-width: 600px) {
+    width: 510px;
+    height: 660px;
+  }
+
+  /* @media (min-width: 700px) {
+    width: 595px;
+    height: 770px;
+  } */
+
+  /* @media (min-width: 800px) {
+    width: 680px;
+    height: 880px;
+  } */
+
+  /* @media (min-width: 900px) {
+    width: 765px;
+    height: 990px;
+  } */
+
+  /* @media (min-width: 1000px) {
+    width: 850px;
+    height: 1100px;
+  } */
+
+  /* @media (min-width: 1100px) {
+    width: 935px;
+    height: 1210px;
+  } */
+
+  /* @media (min-width: 1200px) {
+    width: 1020px;
+    height: 1320px;
+  } */
 
   display: flex;
   flex-direction: column;
   align-items: center;
 
+  margin-bottom: 2rem;
+
   @media print {
-    min-height: 900px;
     width: 100%;
     margin: 0;
     border: 0;
