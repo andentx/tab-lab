ac2d101f02243ab48a79bcd389be0e189d2b0480
Andrew D <56039221+andent2@users.noreply.github.com>
2022-12-23 10:45:24 -0500

    Move favicon to head in layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import styled from 'styled-components';
 
 import Color from '../components/Color';
@@ -39,6 +41,19 @@ const MainContent = styled.main`
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <meta name='description' content='tab lab - development' />
+
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='shortcut icon' href='/icons/favicon.ico' />
+        <link rel='icon' type='image/png' sizes='32x32' href='icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='icons/favicon-16x16.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='icons/apple-touch-icon.png' />
+        <meta name='msapplication-TileColor' content='#008877' />
+        <meta name='msapplication-config' content='icons/browserconfig.xml' />
+        <link rel='mask-icon' href='icons/safari-pinned-tab.svg' color='#008877' />
+        <link rel='manifest' href='icons/site.webmanifest' />
+      </Head>
       <Color />
       <PageContainer>
         <MainContent>{children}</MainContent>
